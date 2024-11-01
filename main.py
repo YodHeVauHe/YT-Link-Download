dd7d1592e376f9eedfda0d6cdc18ce2f0a372c18
Yoddeed <43342618+YodHeVauHe@users.noreply.github.com>
2024-11-01 10:44:29 +0000

    updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 # Set page config
-st.set_page_config(page_title="🎥 YouTube Link Download", layout="wide")
+st.set_page_config(page_title= "YouTube Link Download", layout="wide")
 
 st.title("⇣ 🎥 YouTube Link Download")
